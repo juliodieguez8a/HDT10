eabf23fa5e35981e987eead3f534d4123ec01af0
juliodieguez8a <juliodieguez8a@gmail.com>
2015-11-08 21:30:42 -0600

    :)

---
+++ src/Archivo.java
@@ -0,0 +1,50 @@
+import java.io.*;
+
+public class Archivo{
+    
+    File doc;
+    FileReader fr;
+    BufferedReader br;
+    FileWriter fw;
+    PrintWriter pw;
+    grafo_interfaz grafo = new matriz();
+
+    public void obtenerArchivo(String direccion) throws FileNotFoundException{
+       doc=new File(direccion); 
+    }  
+    
+    public grafo_interfaz arregloNombres() throws IOException{
+        // Lectura del fichero
+        fr = new FileReader (doc);
+        br = new BufferedReader(fr);
+        String linea;
+        while((linea=br.readLine())!=null){
+            String[] tmp;
+            tmp=linea.split(" ");
+            grafo.add(tmp[0]);
+            grafo.add(tmp[1]);
+        }
+        return grafo;
+    }
+    
+    public void write(String cadena) throws IOException{
+        fw = new FileWriter(doc);
+        pw = new PrintWriter(fw);
+        pw.println(cadena);
+    }
+    
+    public grafo_interfaz matrizCostos() throws IOException{
+        // Lectura del fichero
+        fr = new FileReader (doc);
+        br = new BufferedReader(fr);
+        String linea;
+
+        while((linea=br.readLine())!=null){
+            String[] tmp;
+            tmp=linea.split(" ");
+            grafo.addEdge(tmp[0], tmp[1], tmp[2]);
+        }
+        return grafo;
+    }
+
+}
---
+++ src/grafo_interfaz.java
@@ -0,0 +1,19 @@
+public interface grafo_interfaz<V,E> {
+
+    public void add(V label);
+
+    public void addEdge(V vtx1, V vtx2, E label);
+
+    public void show();
+
+    public int getIndex(V label);
+
+    public V get(int label);
+    
+    public int getEdge(V label1, V label2);
+    
+    public boolean contains(V label);
+    
+    public int size();
+    
+}
